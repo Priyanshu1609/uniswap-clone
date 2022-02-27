b7d34c71833306f6d786ee0cfdc31016ab3ffc56
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-02-28 02:11:51 +0530

    first

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/VsUuFQTF1nb_Vri2VoJeVZZICzP6F3gN',
-      accounts: ['9916b9f39c79ea56ad8e805fc4ed4356fcd93fe6f8721399d866c0c4d9db06e2']
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/',
+      accounts: ['']
     },
   },
 }
