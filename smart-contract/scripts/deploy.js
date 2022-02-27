f3b4a3b180272b922d2aa4175f0fd6142ebb1b01
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-02-28 02:08:04 +0530

    first

---
+++ smart-contract/scripts/deploy.js
@@ -0,0 +1,17 @@
+
+const main = async () => {
+  const transactionFactory = await hre.ethers.getContractFactory("Transactions");
+  const transactionContract = await transactionFactory.deploy();
+
+  await transactionContract.deployed();
+
+  console.log("Greeter deployed to:", transactionContract.address);
+}
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
